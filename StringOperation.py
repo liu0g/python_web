f48dae662dcf5adc34641ac56602420482a9986f
liugang <915395079@qq.com>
2015-09-29 10:31:01 +0800

    test

--- StringOperation.py
+++ StringOperation.py
@@ -1,8 +1,11 @@
 #encoding = utf-8
 __author__ = 'lg'
 
-list1 = ['java','python','ruby','perl','mac']
-list2 = ['linux','mac','windows','ruby']
+with open('H:\Programming\Python_Workspace\in.txt','r') as f:
+    list1 = f.readline().split()
+    list2 = f.readline().split()
+    print(list1)
+    print(list2)
 
 #两个list的交集(法一) 时间复杂度为O(n^2)
 def intersect(a,b):
@@ -12,16 +15,17 @@ def intersect(a,b):
             if a[i] == b[j]:
                 if a[i] not in listRes:
                     listRes.append(a[i])
-    return listRes
+    listRes.sort()
+    print(listRes)
 
-#两个list的交集（法二）　　时间复杂度为O(n)
+#两个list的交集（法二）　　时间复杂度为O(n^2)
 def intersect_1(a,b):
     listRes = []
     for i in range(len(a)):
         if a[i] in b:
             if a[i] not in listRes:
                 listRes.append(a[i])
-    return listRes
+    print(sorted(listRes))
 
 #两个list的差集
 def minus(a,b):
@@ -29,27 +33,14 @@ def minus(a,b):
     for i in range(len(a)):
         if a[i] not in b:
             listRes.append(a[i])
-    return listRes
-
-# 按字母表输出字符串
-def alphabet_output(listPram):
-    sortedList = []
-    for i in range(len(listPram)):
-        sortedStrRes = ''
-        sortedStrList = sorted(listPram[i])
-        for j in range(len(sortedStrList)):
-            sortedStrRes += sortedStrList[j]
-        sortedList.append(sortedStrRes)
-    print(sortedList)
+    print(sorted(listRes))
 
 # list1 intersect list2
-intersectList = intersect_1(list1,list2)
-alphabet_output(intersectList)
+intersect(list1,list2)
 
 # list1 minus list2
-minusList = minus(list1,list2)
-alphabet_output(minusList)
+minus(list1,list2)
 
 # list2 minus list1
-minusList_1 = minus(list2,list1)
-alphabet_output(minusList_1)
+minus(list2,list1)
+
